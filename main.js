9e86a8a34938dcd520d10ad338bd1759afc5153f
kaczenka <75174671+kaczenka@users.noreply.github.com>
2021-03-27 16:57:26 +0100

    hra-virtualni-kostka

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // tady je místo pro náš program
+function hazej() {
+    let kostka = document.querySelector("#kostka");
+    let audio = document.querySelector("#audio");
+    let text = document.querySelector("#zprava");
+
+    let vysledek = Math.floor(Math.random() * 6) + 1;
+    if (vysledek >= 1 && vysledek < 6) {
+        kostka.src = 'obrazky/' + vysledek + '.png';
+    }
+
+    else if (vysledek === 6) {
+        kostka.src = 'obrazky/6.png';
+        audio.play();
+        zprava.textContent = 'Gratuluji, jsi vítěz!';
+    }
+   console.log(vysledek);
+}
